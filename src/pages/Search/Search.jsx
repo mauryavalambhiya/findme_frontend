f414821ce1183a56a5ec039cd6580da8b31097a6
mauryavalambhiya <mvalambhiya737@rku.ac.in>
2024-03-16 13:46:56 +0530

    stable version 3 in search.jsx two function to get data has been fix

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,87 +1,59 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 // import SearchBar from "../../components/SearchBar";
-import axios  from "axios";
 import BlankCard from "../../components/BlankCard";
-
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 const Search = () => {
-  const [locparams, setLocParams] = useState(null);
+  const [locparams, setLocParams] = useState(null);  // save the location info
   const [mainparams, setMainParams] = useState(null);
   const [subparams, setSubParams] = useState(null);
   const [qparams, setQParams] = useState(null);
-  const [pageLoded, setPageLoded] = useState(false)
+  const [pageLoded, setPageLoded] = useState(false);
   var [searchdata, setSearchData] = useState(null);
   const [searchInput, setSearchInput] = useState("");
   const navigate = useNavigate();
+  const axiosPrivate = useAxiosPrivate();
 
   // const [locparams, setLocParams] = useState(null);
   const location = useLocation();
 
-  function getData(query, maincat, subcat){
-    console.log("Get data :- " + query + " " + maincat + " " + subcat)
-
-    let data = JSON.stringify({
-      "search_query": query + " " + maincat + " " + subcat
-    });
-    
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'http://localhost:5000/v1/user/search',
-      headers: { 
-        'ORIGIN_PRIVATE': 'x078672399', 
-        'API_KEY': 'API-kkj234rKJEWFI4HV3YCH84N7TNC8T', 
-        'Content-Type': 'application/json', 
-        'Cookie': 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX3Bob25lbm8iOiI5OTc4OTE2NDk5IiwiaWF0IjoxNzA4Njg5MTM3LCJleHAiOjE3MDg3NzU1Mzd9.o1-0nQ_D54uF1U1u9kxp-IX3aXtzORyt0JWCrOnctNw'
+  // search by main and sub category
+  async function getData(query, maincat, subcat) {
+    console.log("Get data :- " + query + " " + maincat + " " + subcat);
+    const response = await axiosPrivate.post(
+      "http://localhost:5000/v1/user/search",
+      {
+        search_query: query + " " + maincat + " " + subcat,
       },
-      data : data
-    };
-    
-    axios.request(config)
-    .then((response) => {
-      setSearchData()
-      setSearchData(response.data)
-      // console.log(JSON.stringify(response.data.profile_id_list));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-
+      {
+        headers: {
+          origin_private: "x078672399",
+          API_KEY: 'API-kkj234rKJEWFI4HV3YCH84N7TNC8T', 
+        },
+      }
+    );
+    setSearchData(response.data)
   }
 
-  function getLocData(query, maincat, subcat, location){
+  async function getLocData(query, maincat, subcat, location) {
     // locparams
-    let data = JSON.stringify({
-      "search_query": `${query} ${maincat} ${subcat}`,
-      "gps_location" : location
-
-    });
-
-    console.log("DATA :-- "  + data);
-    
-    let config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'http://localhost:5000/v1/user/search',
-      headers: { 
-        'ORIGIN_PRIVATE': 'x078672399', 
-        'API_KEY': 'API-kkj234rKJEWFI4HV3YCH84N7TNC8T', 
-        'Content-Type': 'application/json', 
-        'Cookie': 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX3Bob25lbm8iOiI5OTc4OTE2NDk5IiwiaWF0IjoxNzA4Njg5MTM3LCJleHAiOjE3MDg3NzU1Mzd9.o1-0nQ_D54uF1U1u9kxp-IX3aXtzORyt0JWCrOnctNw'
-      },
-      data : data
+    let data = {
+      search_query: `${query} ${maincat} ${subcat}`,
+      gps_location: location,
     };
-    
-    axios.request(config)
-    .then((response) => {
-      setSearchData()
-      setSearchData(response.data)
-      // console.log(JSON.stringify(response.data.profile_id_list));
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+
+    const response = await axiosPrivate.post(
+      "http://localhost:5000/v1/user/search",
+      data,
+      {
+        headers: {
+          origin_private: "x078672399",
+          API_KEY: 'API-kkj234rKJEWFI4HV3YCH84N7TNC8T', 
+        },
+      }
+    );
+    setSearchData(response.data)
   }
 
   useEffect(() => {
@@ -91,29 +63,29 @@ const Search = () => {
     const qValue = queryParams.get("q");
     const mainValue = queryParams.get("maincat");
     const subValue = queryParams.get("subcat");
-    if(!qValue){
-      setQParams("")
-    }else{
+    if (!qValue) {
+      setQParams("all");
+    } else {
       setQParams(qValue);
     }
     if (!mainValue) {
       setMainParams("");
-    }else{
+    } else {
       setMainParams(mainValue);
     }
     if (!subValue) {
       setSubParams("");
-    }else{
+    } else {
       setSubParams(subValue);
     }
 
-    if( latValue != null || longValue != null){
-      setLocParams([22.2873299, 70.7986046])
-    }else{
-      setLocParams([latValue,longValue])
+    if (latValue != null || longValue != null) {
+      setLocParams([22.2873299, 70.7986046]);
+    } else {
+      setLocParams([latValue, longValue]);
     }
 
-    setPageLoded(true)
+    setPageLoded(true);
   }, []);
 
   useEffect(() => {
@@ -140,8 +112,6 @@ const Search = () => {
   //   }, [pageLoded])
   // }
 
-
-
   // useEffect(() => {
   //   console.log("searchdata :- " +  searchdata)
   // }, [searchdata])
@@ -149,33 +119,45 @@ const Search = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     navigate(`/search?q=${searchInput}`, {
-      replace: false,
+      replace: true,
     });
-  }
+  };
 
   return (
-    <div className=" "> 
-      <form  className=" flex align-middle justify-start items-center mx-10" onSubmit={handleSearch}>
-        <input className=" px-2 rounded-lg h-14 text-2xl border-gray-200 border-2 w-full mx-4" type="text" placeholder="Search anything" onChange={(e) => { setSearchInput(e.target.value.toString()) }}></input>
-        <button className=" p-4 bg-orange text-white text-lg rounded-lg  mx-4">Search</button>
+    <div className=" ">
+      <form
+        className=" flex align-middle justify-start items-center mx-10"
+        onSubmit={handleSearch}
+      >
+        <input
+          className=" px-2 rounded-lg h-14 text-2xl border-gray-200 border-2 w-full mx-4"
+          type="text"
+          placeholder="Search anything"
+          onChange={(e) => {
+            setSearchInput(e.target.value.toString());
+          }}
+        ></input>
+        <button className=" p-4 bg-orange text-white text-lg rounded-lg  mx-4">
+          Search
+        </button>
       </form>
       <div className="w-full p-4">
-      {
-        searchdata === null
-        ? <p>Nothing to show</p>
-        : searchdata['profile_id_list'].map((item, index) => (
-          <BlankCard 
-            key={index}
-            business_name={item.business_name}
-            business_image={item.business_image} 
-            business_address={item.business_address} 
-            main_category={item.main_category}
-            business_number={item.business_number}
-            rating={4}
-          />
-        ))
-      }
-    </div>
+        {searchdata === null ? (
+          <p>Nothing to show</p>
+        ) : (
+          searchdata["profile_id_list"].map((item, index) => (
+            <BlankCard
+              key={index}
+              business_name={item.business_name}
+              business_image={item.business_image}
+              business_address={item.business_address}
+              main_category={item.main_category}
+              business_number={item.business_number}
+              rating={4}
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
